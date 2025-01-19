ec44713bb52bf2850ba3908ff4e7885680214fc8
bobby <blahblah@gmail>
2025-01-19 16:49:54 +1100

    Update tcp_server.py - Added Methods to Start/Stop/Check if a port is listening

--- tcp_dummy/tcp_server.py
+++ tcp_dummy/tcp_server.py
@@ -1,14 +1,43 @@
 import socket
+from typing import Dict
 
 
 class TCPServer():
 
     def __init__(self, server_ip: str = ''):
-        self.listening_ports = {}
+        self.listening_ports: Dict[int, socket.socket | None] = {}
         self.server_ip = server_ip
+        print(f'[*] Created Server [IP: {self.server_ip}]')
 
     def get_server_ip(self) -> str:
         return self.server_ip
     
     def set_server_ip(self, new_ip: str) -> None:
         self.server_ip = new_ip
+        print(f'[*] New Server IP: {self.server_ip}')
+
+    def is_listening(self, port_number: int) -> bool:
+        if self.listening_ports[port_number] is None:
+            return False
+        else:
+            return True
+        
+    def start_listener(self, port_number: int) -> None:
+        if self.is_listening(port_number):
+            return None
+        
+        self.listening_ports[port_number] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.listening_ports[port_number].bind((self.server_ip, port_number))
+
+        self.listening_ports[port_number].listen(0)
+
+        print(f'[*] Listening on {self.server_ip}:{port_number}')
+
+    def stop_listener(self, port_number: int) -> None:
+        if not self.is_listening(port_number):
+            return None
+        
+        self.listening_ports[port_number].close()
+        self.listening_ports[port_number] = None
+        print(f'[*] Port Closed: {port_number}')
