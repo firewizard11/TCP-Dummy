da011a8c6b33c2fc535ea882dab9ba93472623ed
Sam Kaveh <seriousemail178@gmail.com>
2025-01-19 23:29:40 +1100

    Update tcp_client.py - Added Print to __init__ - Improved readability of is_connected - renamed host_ip to server_ip - renamed message to data - Added start of process prints to creating and closing conenctions - Added except for connections  being refused - Added is_connected check before closing connections - Updated timeout exception message in receive_data

--- tcp_dummy/tcp_client.py
+++ tcp_dummy/tcp_client.py
@@ -6,6 +6,7 @@ class TCPClient():
     def __init__(self, timeout: int = 10):
         self.current_connection: socket.socket | None = None
         self.timeout = timeout
+        print('[*] Client Created')
 
     # Getters and Setters
     def get_timeout(self) -> int:
@@ -13,50 +14,61 @@ class TCPClient():
     
     def set_timeout(self, new_timeout: int) -> None:
         self.timeout = new_timeout
-    
+
     # Methods
     def is_connected(self) -> bool:
-        return self.current_connection is not None
-        
-    def close_connection(self) -> None:
-        self.current_connection.close()
-        self.current_connection = None
-        print('[*] Connection Closed')
+        if self.current_connection is None:
+            return False
+        else:
+            return True
 
-    def create_connection(self, host_ip: str, port: int) -> None:
+    def create_connection(self, server_ip: str, port: int) -> None:
+        print(f'[*] Creating Connection with {server_ip}:{port}...')
+
         if self.is_connected():
-            print('[*] Closing Current Connection')
+            print('[*] Closing Previous Connection...')
             self.close_connection()
 
         new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         new_sock.settimeout(self.timeout)
 
         try:
-            new_sock.connect((host_ip, port))
+            new_sock.connect((server_ip, port))
         except TimeoutError:
-            print(f'[!] Connection Timeout with {host_ip}:{port}')
+            print(f'[!] Connection Timeout with {server_ip}:{port}')
+            new_sock.close()
+            return None
+        except ConnectionRefusedError:
+            print(f'[!] Connection Refused by {server_ip}')
             new_sock.close()
             return None
         
         self.current_connection = new_sock
-        print(f'[*] Connection Established with {host_ip}:{port}')
+        print(f'[*] Connection Established with {server_ip}:{port}') 
 
-    def send_message(self, message: str) -> None:
+    def close_connection(self) -> None:
         if not self.is_connected():
-            print('[!] No Connection Active')
+            return None
+
+        print('[*] Closing Connection...')
+        self.current_connection.close()
+        self.current_connection = None
+        print('[*] Connection Closed')
+
+    def send_data(self, data: str) -> None:
+        if not self.is_connected():
             return None
         
         try:
-            self.current_connection.sendall(bytes(message, encoding='utf-8'))
+            self.current_connection.sendall(bytes(data, encoding='utf-8'))
         except TimeoutError:
-            print('[!] Connection Timeout: Message Didn\'t Send')
+            print('[!] Connection Timeout: Data Didn\'t Send')
             return None
         
-        print(f'[*] Outgoing Message: {message}')
+        print(f'[*] Outgoing Data: {data}')
 
-    def receive_message(self) -> None:
+    def receive_data(self) -> None:
         if not self.is_connected():
-            print('[!] No Connection Active')
             return None
 
         while True:
@@ -64,25 +76,19 @@ class TCPClient():
             try:
                 data = self.current_connection.recv(4096)
             except TimeoutError:
-                print('[*] No Response From Server')
+                print('[!] Timeout: No Response From Server')
                 break
 
             if not data:
                 break
 
-            print(f'[*] Incoming Message: {data.decode('utf-8')}')
+            print(f'[*] Incoming Data: {data.decode('utf-8')}')
 
 if __name__ == '__main__':
-    test_client = TCPClient(1)
-
-    print(test_client.is_connected(), 'timeout:', test_client.get_timeout())
-
-    test_client.create_connection('127.0.0.1', 4444)
-
-    test_client.send_message('Hello, Server\n')
-
-    test_client.receive_message()
-
-    test_client.send_message('No Reply?\n')
-
+    test_client = TCPClient()
+    test_client.create_connection('192.168.60.129', 4444)
+    test_client.send_data('Data Test 1!!!!')
+    test_client.create_connection('192.168.60.129', 1234)
+    test_client.send_data('Data Test 2 :::::)_)_)_()()()()')
+    test_client.receive_data()
     test_client.close_connection()
