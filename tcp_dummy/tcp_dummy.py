e79a49e0e836e7207cfa4e93acaab71ee118a212
Sam Kaveh <seriousemail178@gmail.com>
2025-01-23 21:29:45 +1100

    CHANGELOG: - Added command argument to parser and implemented the logic for it
    TODO:
    - Add Object Serialisation???
      - Reason: If we want to implement the close command we need to keep
        the device object between executions
      - Reason 2: If we want to implement replay we need to keep the server
        or client running
    - Add Options for Object creation (timeout, backlog, server_ip)
      - Reason: Allow the user to set their own options
    - Figure out how the cli should interact with the server/client while
      its running

--- tcp_dummy/tcp_dummy.py
+++ tcp_dummy/tcp_dummy.py
@@ -6,12 +6,29 @@ import tcp_server
 def main():
     parser = argparse.ArgumentParser(usage='python3 tcp_dummy.py [command] [options]')
     
-    parser.add_argument('command', choices=['server', 'client'])
+    parser.add_argument('command', choices=['server', 'client', 'close'])
 
-    parser.add_argument('-h --host')
+    args = parser.parse_args()
 
+    command: str = args.command
 
-    
+    print(f'Command: {command}')
+
+    match(command.lower()):
+        case 'server':
+            device = tcp_server.TCPServer()
+        case 'client':
+            device = tcp_client.TCPClient()
+        case 'close':
+            if isinstance(device, tcp_server.TCPServer):
+                device.close_server()
+                return
+            if isinstance(device, tcp_client.TCPClient):
+                device.close_connection()
+                return
+        
+        
+
 
 if __name__ == '__main__':
     main()
