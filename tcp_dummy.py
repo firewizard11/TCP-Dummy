7441ea58890a5be91de123f1462357e9d46e0185
Sam Kaveh <seriousemail178@gmail.com>
2025-02-16 18:40:22 +1100

    Removed Close OPtion

--- tcp_dummy.py
+++ tcp_dummy.py
@@ -6,14 +6,14 @@ import tcp_server
 def main():
     """ Example Flow
     1. Start Server/Client, with relevant options
-    2. Close Server/Client that was opened by previous command
+    2. Ctrl+C to Close the Program
     """
 
     parser = argparse.ArgumentParser(
         prog="tcp_dummy.py"
     )
 
-    parser.add_argument("command", choices=["server", "client", "close"])
+    parser.add_argument("command", choices=["server", "client"])
     parser.add_argument("-H", "--Host")
     parser.add_argument("-P", "--Ports")
 
@@ -32,27 +32,6 @@ def main():
 
         case "client":
             device = tcp_client.TCPClient()
-
-        case "close":
-            if device is None:
-                print("[!] No Device On")
-                return None
-            
-            if type(device) is tcp_server.TCPServer:
-                print("[*] Closing Server...")
-
-                device.close_server()
-
-                print("[*] Server Closed")
-                return None
-            
-            if type(device) is tcp_client.TCPClient:
-                print("[*] Closing Client...")
-
-                device.close_connection()
-
-                print("[*] Client Closed")
-                return None
 
 if __name__ == '__main__':
     main()
