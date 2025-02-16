39d3be5058ab28924eff3ca41a982c8d370e95c4
Sam Kaveh <seriousemail178@gmail.com>
2025-02-16 21:39:38 +1100

    Added Basic Host and Ports Option for client and server

--- tcp_dummy.py
+++ tcp_dummy.py
@@ -10,14 +10,50 @@ if __name__ == '__main__':
     )
 
     parser.add_argument('command', choices=['server', 'client'])
+    parser.add_argument('-H', '--Host')
+    parser.add_argument('-P', '--Ports')
 
     args = parser.parse_args()
 
+    print(args)
+
     command = args.command
+
+    if args.Host is None:
+        host = ''
+    elif validate_host(args.Host):
+        host = args.Host
+    else:
+        print('Error: Host is Invalid')
+        exit()
+
+    try:
+        ports = format_ports(args.Ports)
+    except ValueError as e:
+        print(f'Error: {e}')
+        exit()
 
     match (command):
         case 'server':
-            device = tcp_server.TCPServer()
+            device = tcp_server.TCPServer(host)
+
+            try:
+                for port in ports:
+                    device.open_port(port)
+            except OSError:
+                print('Error: Please use an IP on your device')
+                exit()
+
+            if len(ports) == 1:
+                device.accept_connection(ports[0])
+            else:
+                device.accept_multiple_connections(ports)
 
         case 'client':
             device = tcp_client.TCPClient()
+
+            if len(ports) > 1:
+                print('Error: Too Many Ports')
+                exit()
+            
+            device.create_connection(host, ports[0])
